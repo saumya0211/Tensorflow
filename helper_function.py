c657adec847d4749edca7e3cac6454e4672383f8
saumya0211 <30955390+saumya0211@users.noreply.github.com>
2022-08-12 19:12:09 +0530

    Update helper_function.py

--- helper_function.py
+++ helper_function.py
@@ -322,46 +322,6 @@ def view_random_image(target_dir, target_class):
 
     return img
 
-import cv2
-def check_images( s_dir, ext_list ,val_dir):
-    bad_images=[]
-    bad_ext=[]
-    s_list= os.listdir(s_dir)
-    for klass in s_list:
-        klass_path=os.path.join (s_dir, klass)
-        print ('processing class directory ', klass)
-        if os.path.isdir(klass_path):
-            file_list=os.listdir(klass_path)
-            for f in file_list:
-                f_path=os.path.join (klass_path,f)
-                index=f.rfind('.')
-                ext=f[index+1:].lower()
-                if ext not in ext_list:
-                    print('file ', f_path, ' has an invalid extension ', ext)
-                    bad_ext.append(f_path)
-                if os.path.isfile(f_path):
-                    try:
-                        img=cv2.imread(f_path)
-                        shape=img.shape
-                    except:
-                        print('file ', f_path, ' is not a valid image file')
-                        bad_images.append(f_path)
-                else:
-                    print('*** fatal error, you a sub directory ', f, ' in class directory ', klass)
-        else:
-            print ('*** WARNING*** you have files in ', s_dir, ' it should only contain sub directories')
-    return bad_images, bad_ext
-
-source_dir = val_dir
-good_exts=['jpg', 'png', 'jpeg', 'gif', 'bmp' ] # list of acceptable extensions
-bad_file_list, bad_ext_list=check_images(source_dir, good_exts)
-if len(bad_file_list) !=0:
-    print('improper image files are listed below')
-    for i in range (len(bad_file_list)):
-        print (bad_file_list[i])
-else:
-    print(' no improper image files were found')
-
 def create_modelcheckpoint_callback(filepath):
   modelcheckpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath = filepath,
                                                                 save_weights_only = True,
